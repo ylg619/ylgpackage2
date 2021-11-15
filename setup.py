10e060f7371d9407c4796942ab2489853009ac90
ylg619 <laghrari.yassine@gmail.com>
2021-11-15 14:55:43 +0100

    first push

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='ylgpackage',
+setup(name='ylgpackage2',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
@@ -13,5 +13,5 @@ setup(name='ylgpackage',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/ylgpackage-run'],
+      scripts=['scripts/ylgpackage2-run'],
       zip_safe=False)
